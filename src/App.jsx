f90c6f99a63b34f45dcefb285bb9faeacf93f21b
Orton Bright <ortonb110@gmail.com>
2024-02-06 21:47:37 +0000

    Router setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Error, Home, License, Explore} from './Pages/index';
 
 function App() {
 
   return (
-    <>
-      <p>hello world</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="explore" element={<Explore/>}/>
+        <Route path="license" element={<License/>}/>
+        <Route path="*" element={<Error/>}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/Pages/index.js
@@ -0,0 +1,6 @@
+import Error from "./Error";
+import Home from "./Home";
+import Explore from "./Explore";
+import License from "./License";
+
+export { Error, Home, Explore, License };
