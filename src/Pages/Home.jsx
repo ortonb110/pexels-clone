17a27a66799be5db3bbdda530f206aee533edf6a
Orton Bright <ortonb110@gmail.com>
2024-02-06 22:12:32 +0000

    React Helmet installed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,16 @@
-
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Free Stock Photos, Royalty Free Stock Images & Copyright Free Pictures · Pexels</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      Home
+    </div>
+  );
+};
 
-export default Home
+export default Home;
