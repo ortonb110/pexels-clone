4864d84e3736ff3dce56d1d5c3823e8b59f191c7
Orton Bright <ortonb110@gmail.com>
2024-02-08 23:42:52 +0000

    Refactor

---
+++ src/Components/Header.jsx
@@ -0,0 +1,21 @@
+import NavBar from "./NavBar";
+import SearchComponent from "./SearchComponent";
+
+export const Header = () => {
+  return (
+    <header className="bg-[url('https://images.pexels.com/photos/18267830/pexels-photo-18267830/free-photo-of-close-up-of-red-roses-with-water-drops-on-the-petals.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] h-[56.4vh] bg-no-repeat bg-cover bg-left bg-black bg-blend-overlay bg-opacity-[0.6]">
+      <NavBar
+        borderBottom={false}
+        textColor={"text-white"}
+        backgroundColor={"transparent"}
+      />
+      <section className="flex flex-col justify-center items-center text-white gap-9 mt-[7rem] px-[2rem] md:px-0">
+        <h1 className="text-[2.8rem] font-[600] w-full md:w-[63rem]">
+          The best free stock photos, royalty free images & videos shared by
+          creators.
+        </h1>
+        <SearchComponent formWidth={true} />
+      </section>
+    </header>
+  );
+};
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,6 @@
 import { Helmet } from "react-helmet";
 import { Fragment } from "react";
-import { useEffect } from "react";
-import NavBar from "../Components/NavBar";
-import SearchComponent from "../Components/SearchComponent";
+import { Header } from "../Components/Header";
 
 const Home = () => {
   return (
@@ -15,19 +13,8 @@ const Home = () => {
         </title>
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
-      <main className="bg-[url('https://images.pexels.com/photos/18267830/pexels-photo-18267830/free-photo-of-close-up-of-red-roses-with-water-drops-on-the-petals.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] h-[56.4vh] bg-no-repeat bg-cover bg-left bg-black bg-blend-overlay bg-opacity-[0.6]">
-        <NavBar
-          borderBottom={false}
-          textColor={"text-white"}
-          backgroundColor={"transparent"}
-        />
-        <header className="flex flex-col justify-center items-center text-white gap-9 mt-[7rem] px-[2rem] md:px-0">
-          <h1 className="text-[2.8rem] font-[600] w-full md:w-[63rem]">
-            The best free stock photos, royalty free images & videos shared by
-            creators.
-          </h1>
-          <SearchComponent formWidth={true}/>
-        </header>
+      <main >
+        <Header/>
       </main>
     </Fragment>
   );
