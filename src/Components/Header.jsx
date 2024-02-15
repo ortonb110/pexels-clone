a10bc6eda9e13faf46dbf426e912a92886315f92
Orton Bright <ortonb110@gmail.com>
2024-02-15 20:27:30 +0000

    Header Section Complete without issues

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,21 @@
 import NavBar from "./NavBar";
 import SearchComponent from "./SearchComponent";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Header = () => {
-
+  const [result, setResult] = useState('bg-black');
+  const [name, setName] = useState('undefined');
   const fetchWallpaper = async () => {
     try {
-      const data = await axios.get(
-        "https://api.pexels.com/v1/curated",
-        {
-          headers: {
-            Authorization:
-              "BpcIrcNPB6GY1jcLDdlPYgAmhEE7E4Xhz9hm86g830nuKTLk39GHqurs",
-          },
-        }
-      );
-
-      console.log(data);
+      const {data} = await axios.get("https://api.pexels.com/v1/curated", {
+        headers: {
+          Authorization:
+            "BpcIrcNPB6GY1jcLDdlPYgAmhEE7E4Xhz9hm86g830nuKTLk39GHqurs",
+        },
+      });
+      setResult(data?.photos[0]?.src?.landscape);
+      setName(data?.photos[0]?.photographer);
     } catch (error) {
       console.log(error);
     }
@@ -27,19 +25,26 @@ export const Header = () => {
   }, []);
 
   return (
-    <header className="bg-[url('https://images.pexels.com/photos/18267830/pexels-photo-18267830/free-photo-of-close-up-of-red-roses-with-water-drops-on-the-petals.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] h-[56.4vh] bg-no-repeat bg-cover bg-left bg-black bg-blend-overlay bg-opacity-[0.6]">
+    <header className={`relative bg-[url('${result}')] min-h-[56.4vh] bg-no-repeat bg-cover bg-left bg-black bg-blend-overlay bg-opacity-[0.6]`}>
       <NavBar
         borderBottom={false}
         textColor={"text-white"}
         backgroundColor={"transparent"}
       />
       <section className="flex flex-col justify-center items-center text-white gap-9 mt-[7rem] px-[2rem] md:px-0">
-        <h1 className="text-[3.2rem] font-[600] w-full md:w-[63rem]">
+        <h1 className=" text-[2.8rem] md:text-[3.2rem] font-[600] w-full md:w-[63rem]">
           The best free stock photos, royalty free images & videos shared by
           creators.
         </h1>
         <SearchComponent formWidth={true} />
       </section>
+      <a
+        href="#"
+        className="absolute right-[2rem] bottom-[2.5rem] text-[1.5rem] md:text-[2rem] space-x-2 font-[600] flex"
+      >
+        <p className="text-gray-500">Photo by</p>
+        <p className="text-gray-300 capitalize">{name}</p>
+      </a>
     </header>
   );
 };
