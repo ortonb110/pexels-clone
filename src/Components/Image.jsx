a5bdf14124cc1ff77601a3fff4a3007da9a1d115
Orton Bright <ortonb110@gmail.com>
2024-02-15 23:00:34 +0000

    Created Image component

---
+++ src/Components/Image.jsx
@@ -0,0 +1,13 @@
+import sampleImage from  '../assets/SampleImages/sample.webp';
+
+const Image = () => {
+  return (
+    <div >
+        <article className={`bg-[url('https://images.pexels.com/photos/20135107/pexels-photo-20135107/free-photo-of-mueller-hut-adventure.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-contain w-full  bg-no-repeat`}>
+            <h1>hello</h1>
+        </article>
+    </div>
+  )
+}
+
+export default Image
--- src/Components/imageLoader.jsx
+++ src/Components/imageLoader.jsx
@@ -1,13 +1,20 @@
 import { Fragment, useEffect, useState } from "react";
+import Masonry from "masonry-layout";
+import Image from "./Image";
 
 const ImageLoader = () => {
+    const [images, setImages] = useState()
+
+
+
+
   return (
     <Fragment>
       <div>
         <h2>free stock photos</h2>
       </div>
       <section>
-        
+        <Image/>
       </section>
     </Fragment>
   );
