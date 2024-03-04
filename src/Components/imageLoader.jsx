1649c5c36031e66d252e03aa3c6517c693526cfe
Kobe Dev <ortonb110@gmail.com>
2024-03-04 08:42:37 +0000

    Category selector

--- src/Components/imageLoader.jsx
+++ src/Components/imageLoader.jsx
@@ -1,22 +1,14 @@
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Masonry from "masonry-layout";
 import Image from "./Image";
 
 const ImageLoader = () => {
-    const [images, setImages] = useState()
-
-
-
+  const [images, setImages] = useState();
 
   return (
-    <Fragment>
-      <div>
-        <h2>free stock photos</h2>
-      </div>
-      <section>
-        <Image/>
-      </section>
-    </Fragment>
+    <section>
+      <Image />
+    </section>
   );
 };
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,20 +3,18 @@ import { Fragment, useEffect, useState } from "react";
 import { Header } from "../Components/Header";
 import ImageLoader from "../Components/imageLoader";
 import NavBar from "../Components/NavBar";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   const [show, setShow] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      if(window.scrollY > (560)) {
+      if (window.scrollY > 600) {
         //Add display show class to Navigation bar when window scroll is greater than 560px
         setShow(true);
-      }
-       else {
-        
+      } else {
         setShow(false);
-       }
-      
+      }
     });
   }, []);
 
@@ -31,7 +29,7 @@ const Home = () => {
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
       <main>
-        <Header/>
+        <Header />
         <NavBar
           searchBar={true}
           backgroundColor={"white"}
@@ -41,6 +39,12 @@ const Home = () => {
           fixToTop={true}
         />
         <section className="px-[10rem] pt-[5rem] pb-[2rem]">
+          <div>
+            <NavLink to={"/home"}>home</NavLink>
+            <NavLink to={"/videos"}>video</NavLink>
+            <NavLink to={"/home"}>leaderboard</NavLink>
+            <NavLink to={"/home"}>challenges</NavLink>
+          </div>
           <ImageLoader />
         </section>
       </main>
