fdf993cc6707f4aef7c066ce186b6854c34fa03f
Kobe Dev <ortonb110@gmail.com>
2024-03-05 09:25:20 +0000

    Animating loader

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,30 +1,24 @@
-import { motion, stagger } from "framer-motion";
+import gsap from "gsap";
+import { useEffect } from "react";
 
 const Loader = () => {
+//   useEffect(() => {
+//     gsap.to(".loader", {
+//       duration: 2,
+//       ease: "slow(0.7,0.7,false)",
+//       x: 1000,
+//       stagger: 0.2,
+//       repeat: Infinity
+//     });
+//   }, []);
+
   return (
-    <motion.div
-      initial={{
-        x: -150,
-        opacity: 0.2
-      }}
-
-      animate={{
-        opacity: 1,
-        x: 900,
-      }}
-      transition={{
-        staggerChildren: 2
-      }}
-
-      
-
-      className="flex space-x-5 absolute bottom-8"
-    >
-      <span className="block w-[2rem] h-[2rem] bg-gray-500 rounded-full"></span>
-      <span className="block w-[2rem] h-[2rem] bg-gray-500 rounded-full"></span>
-      <span className="block w-[2rem] h-[2rem] bg-gray-500 rounded-full"></span>
-      <span className="block w-[2rem] h-[2rem] bg-gray-500 rounded-full"></span>
-    </motion.div>
+    <div className="flex  space-x-5 absolute bottom-8 ">
+      <span className="loader block w-[2rem] h-[2rem] bg-gray-500 rounded-full translate-x-[-300px]"></span>
+      <span className="loader block w-[2rem] h-[2rem] bg-gray-500 rounded-full translate-x-[-300px]"></span>
+      <span className="loader block w-[2rem] h-[2rem] bg-gray-500 rounded-full translate-x-[-300px]"></span>
+      <span className="loader block w-[2rem] h-[2rem] bg-gray-500 rounded-full translate-x-[-300px]"></span>
+    </div>
   );
 };
 
