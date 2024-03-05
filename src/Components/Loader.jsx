f9e396fa4430ba3b7547d860202ef6f5b7ae019f
Kobe Dev <ortonb110@gmail.com>
2024-03-05 22:01:20 +0000

    Minor changes

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -2,15 +2,15 @@ import gsap from "gsap";
 import { useEffect } from "react";
 
 const Loader = () => {
-  useEffect(() => {
-    gsap.to(".loader", {
-      duration: 1.5,
-      ease: "slow(0.7,0.7,false)",
-      x: 2000,
-      stagger: 0.1,
-      repeat: Infinity
-    });
-  }, []);
+//   useEffect(() => {
+//     gsap.to(".loader", {
+//       duration: 1.5,
+//       ease: "slow(0.7,0.7,false)",
+//       x: 2000,
+//       stagger: 0.1,
+//       repeat: Infinity
+//     });
+//   }, []);
 
   return (
     <div className="flex  space-x-5 absolute bottom-8 w-full ">
